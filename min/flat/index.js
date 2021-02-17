defb04b72976841a09d98e7bae90c76cb79cbeb8
João Guilherme <joaogbarbosa@gmail.com>
2021-02-17 13:49:31 -0300

    export default

---
+++ min/flat/index.js
@@ -0,0 +1,31 @@
+import alipay from "./alipay";
+import amex from "./amex";
+import diners from "./diners";
+import discover from "./discover";
+import elo from "./elo";
+import hipercard from "./hipercard";
+import jcb from "./jcb";
+import maestro from "./maestro";
+import mastercard from "./mastercard";
+import paypal from "./paypal";
+import securityCode from "./security-code";
+import unionpay from "./unionpay";
+import verve from "./verve";
+import visa from "./visa";
+
+export default {
+  alipay,
+  amex,
+  diners,
+  discover,
+  elo,
+  hipercard,
+  jcb,
+  maestro,
+  mastercard,
+  paypal,
+  securityCode,
+  unionpay,
+  verve,
+  visa
+};
